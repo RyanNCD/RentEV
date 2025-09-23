e5ec375004c416a79bf1d9b1b2d2e11e458cdb91
RyanNCD <danh.11x2.nguyen@gmail.com>
2025-09-23 21:46:00 +0700

    upload code

---
+++ Backend/Repository/DTO/Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.DTO
+{
+    public class Login
+    {
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string PasswordHash { get; set; }
+    }
+}
